c27767dfec8db24d83bbe8808972ee30ed3eb515
klausmullerDev <klaus.muller@sempreceub.com>
2024-06-08 13:13:04 -0300

    update das funcionalidades

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from funcoes import db, authenticate_user, add_trabalhador, obter_funcionarios_do_banco_de_dados
+from funcoes import db, authenticate_user, add_trabalhador, obter_funcionarios_do_banco_de_dados, \
+    obter_funcionarios_com_filtro
 
 app = Flask(__name__)
 app.secret_key = "chave_secreta"
 
+
 @app.route('/')
 def login():
     return render_template('login.html')
+
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
@@ -23,10 +26,24 @@ def dashboard():
     else:
         return render_template('dashboard.html')
 
-@app.route('/listar_funcionarios')
+
+@app.route('/listar_funcionarios', methods=['GET', 'POST'])
 def listar_funcionarios():
-    funcionarios = obter_funcionarios_do_banco_de_dados()
-    return render_template('listar_funcionarios.html', funcionarios=funcionarios)
+    filtros = {
+        'id': request.form.get('id'),
+        'nome': request.form.get('nome'),
+        'cargo': request.form.get('cargo'),
+        'data_contratacao': request.form.get('data_contratacao'),
+        'salario': request.form.get('salario'),
+        'telefone': request.form.get('telefone'),
+        'email': request.form.get('email')
+    }
+    funcionarios = obter_funcionarios_com_filtro(filtros)
+    total_funcionarios = len(funcionarios)
+    total_salarios = sum(func['salario'] for func in funcionarios)
+    return render_template('listar_funcionarios.html', funcionarios=funcionarios, total_funcionarios=total_funcionarios,
+                           total_salarios=total_salarios)
+
 
 @app.route('/add_trabalhador', methods=['GET', 'POST'])
 def add_trabalhador_route():
@@ -44,6 +61,44 @@ def add_trabalhador_route():
         return redirect(url_for('add_trabalhador_route'))
 
     return render_template('add_trabalhador.html')
+
+
+@app.route('/editar_trabalhador/<int:id>', methods=['GET', 'POST'])
+def editar_trabalhador_route(id):
+    if request.method == 'POST':
+        nome = request.form['nome']
+        cargo = request.form['cargo']
+        data_contratacao = request.form['data_contratacao']
+        salario = request.form['salario']
+        telefone = request.form['telefone']
+        email = request.form['email']
+
+        query = """
+            UPDATE trabalhadores 
+            SET nome=%s, cargo=%s, data_contratacao=%s, salario=%s, telefone=%s, email=%s 
+            WHERE id=%s
+        """
+        db.upd_del(query, [nome, cargo, data_contratacao, salario, telefone, email, id])
+
+        flash('Trabalhador atualizado com sucesso!', 'success')
+        return redirect(url_for('listar_funcionarios'))
+
+    trabalhador = db.get_object("SELECT * FROM trabalhadores WHERE id=%s", [id])
+    return render_template('editar_trabalhador.html', trabalhador=trabalhador)
+
+
+@app.route('/remover_trabalhador/<int:id>', methods=['GET', 'POST'])
+def remover_trabalhador_route(id):
+    if request.method == 'POST':
+        query = "DELETE FROM trabalhadores WHERE id=%s"
+        db.upd_del(query, [id])
+
+        flash('Trabalhador removido com sucesso!', 'success')
+        return redirect(url_for('listar_funcionarios'))
+
+    trabalhador = db.get_object("SELECT * FROM trabalhadores WHERE id=%s", [id])
+    return render_template('remover_trabalhador.html', trabalhador=trabalhador)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- funcoes.py
+++ funcoes.py
@@ -4,11 +4,13 @@ from sql.banco import SQL
 # Instanciando a classe SQL
 db = SQL(servidor='localhost', usr='root', pwd='ceub123456', esquema='test')
 
+
 def authenticate_user(username, password):
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     query = "SELECT * FROM tb_usr WHERE nme_usr=%s AND pwd_usr=%s"
     user = db.get_object(query, [username, hashed_password])
     return user
+
 
 def add_trabalhador(db, nome, cargo, data_contratacao, salario, telefone, email):
     query = """
@@ -17,6 +19,36 @@ def add_trabalhador(db, nome, cargo, data_contratacao, salario, telefone, email)
     """
     db.insert(query, [nome, cargo, data_contratacao, salario, telefone, email])
 
+
 def obter_funcionarios_do_banco_de_dados():
     query = "SELECT * FROM trabalhadores"
     return db.get_list(query)
+
+
+def obter_funcionarios_com_filtro(filtros):
+    query = "SELECT * FROM trabalhadores WHERE 1=1"
+    params = []
+
+    if filtros['id']:
+        query += " AND id = %s"
+        params.append(filtros['id'])
+    if filtros['nome']:
+        query += " AND nome LIKE %s"
+        params.append('%' + filtros['nome'] + '%')
+    if filtros['cargo']:
+        query += " AND cargo LIKE %s"
+        params.append('%' + filtros['cargo'] + '%')
+    if filtros['data_contratacao']:
+        query += " AND data_contratacao = %s"
+        params.append(filtros['data_contratacao'])
+    if filtros['salario']:
+        query += " AND salario = %s"
+        params.append(filtros['salario'])
+    if filtros['telefone']:
+        query += " AND telefone LIKE %s"
+        params.append('%' + filtros['telefone'] + '%')
+    if filtros['email']:
+        query += " AND email LIKE %s"
+        params.append('%' + filtros['email'] + '%')
+
+    return db.get_list(query, params)
